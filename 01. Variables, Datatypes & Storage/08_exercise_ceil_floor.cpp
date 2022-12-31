f1e0c7c80775e98ae3f977d3f19676de2374a8ab
Niladri Ghosh <niladri1406@gmail.com>
2022-12-31 13:06:05 +0530

    feat : added exercise : floor and ceil function

---
+++ 01. Variables, Datatypes & Storage/08_exercise_ceil_floor.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main(){
+    float num = 15.5;
+    cout << "Floor : " << floor(num) <<endl;
+    cout << "Ceil  : " << ceil(num) << endl;
+    return 0;
+}
