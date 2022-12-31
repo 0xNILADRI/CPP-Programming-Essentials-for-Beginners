c09a21e0a72ae34983b81d3ee985e13cbb097dff
Niladri Ghosh <niladri1406@gmail.com>
2022-12-31 12:12:29 +0530

    feat : added intro to variable and datatypes

---
+++ 01. Variables, Datatypes & Storage/01_variable&Datatypes.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a = 10;
+    float b = 20.43;
+    char c = 'A';
+    bool d = true;
+
+    cout << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+    cout << d << endl;
+
+    return 0;
+}
