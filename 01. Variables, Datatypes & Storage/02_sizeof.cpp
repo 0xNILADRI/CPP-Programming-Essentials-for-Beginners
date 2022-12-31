9ddedcb1148baf01606ee567ba3dcfeb41788f06
Niladri Ghosh <niladri1406@gmail.com>
2022-12-31 12:12:59 +0530

    feat : added concept of sizeof operator

---
+++ 01. Variables, Datatypes & Storage/02_sizeof.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a; float b; char c; bool d; double e;
+
+    cout << "SizeOf INT          : " << sizeof(a) << endl;
+    cout << "SizeOf FLOAT        : " << sizeof(b) << endl;
+    cout << "SizeOf CHAR         : " << sizeof(c) << endl;
+    cout << "SizeOf BOOL         : " << sizeof(d) << endl;
+    cout << "SizeOf DOUBLE       : " << sizeof(e) << endl;
+
+    return 0;
+}
