14048b9ad3b617149c40703d8f1e254f41729983
Niladri Ghosh <niladri1406@gmail.com>
2023-07-16 17:33:40 +0530

    add: modified relational operator

--- 02. Operators & Expressions/04_relationalOperators.cpp
+++ 02. Operators & Expressions/04_relationalOperators.cpp
@@ -1,39 +1,27 @@
 #include <iostream>
 using namespace std;
-
-int main() {
-   int a = 21;
-   int b = 10;
-   int c ;
-
-   if( a == b ) {
-      cout << "Line 1 - a is equal to b" << endl ;
-   } else {
-      cout << "Line 1 - a is not equal to b" << endl ;
-   }
+ 
+int main()
+{
+    int a = 6, b = 4;
+ 
+    // Equal to operator
+    cout << "a == b is " << (a == b) << endl;
    
-   if( a < b ) {
-      cout << "Line 2 - a is less than b" << endl ;
-   } else {
-      cout << "Line 2 - a is not less than b" << endl ;
-   }
+    // Greater than operator
+    cout << "a > b is " << (a > b) << endl;
    
-   if( a > b ) {
-      cout << "Line 3 - a is greater than b" << endl ;
-   } else {
-      cout << "Line 3 - a is not greater than b" << endl ;
-   }
+    // Greater than or Equal to operator
+    cout << "a >= b is " << (a >= b) << endl;
    
-   /* Let's change the values of a and b */
-   a = 5;
-   b = 20;
-   if( a <= b ) {
-      cout << "Line 4 - a is either less than or equal to  b" << endl ;
-   }
+    //  Lesser than operator
+    cout << "a < b is " << (a < b) << endl;
    
-   if( b >= a ) {
-      cout << "Line 5 - b is either greater than or equal to b" << endl ;
-   }
+    // Lesser than or Equal to operator
+    cout << "a <= b is " << (a <= b) << endl;
    
-   return 0;
+    // true
+    cout << "a != b is " << (a != b) << endl;
+ 
+    return 0;
 }
