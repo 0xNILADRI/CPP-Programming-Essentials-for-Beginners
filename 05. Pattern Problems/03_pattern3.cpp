1047925b62fcc8f31b4165a645b0abec6487bbe4
Niladri Ghosh <niladri1406@gmail.com>
2023-07-19 22:41:32 +0530

    add : pattern three

---
+++ 05. Pattern Problems/03_pattern3.cpp
@@ -0,0 +1,37 @@
+/*
+
+WAP that reads a positive number N and prints the given pattern for N rows.
+
+Sample Input: 
+==============
+3
+
+Sample Output :
+================
+1
+23
+456
+
+*/
+
+#include <iostream>
+using namespace std;
+
+
+void mainFn() {
+   // write your code here 
+   int n, val=0;
+   cin >> n;
+   for(int i=1; i<=n; i++){
+       for(int j=1; j<=i; j++){
+           val++;
+           cout<<val;
+       }
+       cout<<endl;
+   }
+}
+
+int main(){
+	mainFn();
+	return 0;
+}
