082ce6c1dc325921d923a52b4decd565a0445d38
Niladri Ghosh <niladri1406@gmail.com>
2023-07-19 22:40:55 +0530

    add : pattern one

---
+++ 05. Pattern Problems/01_pattern1.cpp
@@ -0,0 +1,40 @@
+/*
+
+Pattern 1
+==========
+WAP that reads a positive number N and prints the given pattern for N rows.
+
+Sample Input: 
+=============
+4
+
+Sample Output:
+==============
+1
+22
+333
+4444
+
+*/
+
+#include <iostream>
+using namespace std;
+
+
+void mainFn() {
+   // write your code here 
+   int n, val=1;
+   cin >> n;
+   for (int i=1; i<=n; i++){
+       for(int j=1; j<=i; j++){
+           cout<<val;
+       }
+       val++;
+       cout<<endl;
+   }
+}
+
+int main(){
+	mainFn();
+	return 0;
+}
