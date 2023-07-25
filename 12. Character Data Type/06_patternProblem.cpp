a2669adef8d5255d2790ba3bbcef1cac89bae0f5
Niladri Ghosh <niladri1406@gmail.com>
2023-07-26 02:25:43 +0530

    add : AA ABBA ABCCBA ABCDDCBA pattern problem

---
+++ 12. Character Data Type/06_patternProblem.cpp
@@ -0,0 +1,46 @@
+/*
+
+Pattern 6
+==========
+WAP that reads a positive number N and prints the given pattern for N rows.
+
+Sample Input: 
+===============
+4
+
+Sample Output:
+================
+AA
+ABBA
+ABCCBA
+ABCDDBA
+
+*/
+
+#include <iostream>
+using namespace std;
+
+
+void mainFn() {
+   int n;
+   char front, back;
+   cin >> n;
+   for(int i=1; i<=n; i++){
+       front = 'A', back = 'A' + i -1;
+       for(int j=1; j<=i; j++){
+           cout<<(char)front;
+            front++;           
+       }
+       for(int k=1; k<=i; k++){
+           cout<<(char)back;
+           back--;
+       }
+       cout<<endl;
+   }
+   
+}
+
+int main(){
+	mainFn();
+	return 0;
+}
