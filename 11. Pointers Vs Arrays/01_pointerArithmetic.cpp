cb9f2876ef819a26c21aae151affbdeb5f6952ba
Niladri Ghosh <niladri1406@gmail.com>
2023-07-25 21:13:17 +0530

    add : pointer arithmetic

---
+++ 11. Pointers Vs Arrays/01_pointerArithmetic.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int A[5] = { 2, 4, 6, 8, 10 };
+    int *p = A;
+    int *q = &A[3];
+
+    cout << "Addresses of elements of the Array:\n";
+    for (int i = 0; i < 5; i++)
+    {
+        cout << A + i << endl;
+    }
+
+    cout << "\nOperations:\n";
+    p++;
+    cout << "p++:" << p << endl;
+    p--;
+    cout << "p--:" << p << endl;
+    p = p + 2;
+    cout << "p = p + 2:" << p << endl;
+    p = p - 2;
+    cout << "p = p - 2:" << p << endl;
+    int d = q - p;
+    d = d / 2;
+    cout << "d = q - p:" << d << endl;
+
+    return 0;
+}
