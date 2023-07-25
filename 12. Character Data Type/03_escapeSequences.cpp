88430749e237dc42122713af3fc8128a7e4c084e
Niladri Ghosh <niladri1406@gmail.com>
2023-07-26 02:23:25 +0530

    add : print escape sequences

---
+++ 12. Character Data Type/03_escapeSequences.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+ 
+int main(){
+    char a = 'N';
+ 
+    // horizontal tab
+    char b = '\t';
+    char c = 'I';
+    char d = '\t';
+    char e = 'L';
+ 
+    // new line
+    char f = '\n';
+    string s = "is the best";
+    cout << a << b << c << d << e << f << s;
+    return 0;
+}
