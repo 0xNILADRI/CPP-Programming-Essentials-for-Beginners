54a8f34d90779e9f082c5ab1144f0bd777a7b008
Niladri Ghosh <niladri1406@gmail.com>
2023-07-26 02:22:59 +0530

    add : convert ascii value to character

---
+++ 12. Character Data Type/02_asciiValueToCharacter.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+ 
+int main(){
+    int x = 53;
+    cout << "The Corresponding character value of x is : ";
+    cout << char(x) << endl;
+ 
+    x = 65;
+    cout << "The Corresponding character value of x is : ";
+    cout << char(x) << endl;
+ 
+    x = 97;
+    cout << "The Corresponding character value of x is : ";
+    cout << char(x) << endl;
+    return 0;
+}
