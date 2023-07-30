699eecfc8757258e0de72ac9ae76b573e0cca44f
Niladri Ghosh <niladri1406@gmail.com>
2023-07-30 18:20:59 +0530

    add: vector for 1D array

---
+++ 16. Vectors/01_1DVector.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    vector<int> arr = {1,2,3,4,5,6};
+
+    // print values
+    for(auto ele : arr){
+        cout<<ele<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
